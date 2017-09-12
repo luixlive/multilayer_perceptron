dca3f16bf7c2d74e35f19c58253d628df78bb817
Luis Alfonso <luixlive@gmail.com>
2017-09-11 19:13:15 -0500

    Separate command handler in different file

---
+++ libraries/command_handler.py
@@ -0,0 +1,152 @@
+from itertools import product
+from json import loads, dumps
+from shutil import copyfile
+from multilayer_perceptron import learningProcess, getY2
+
+# Constants
+COMMAND_HELP = '--help'
+COMMAND_INPUT = '--input'
+COMMAND_OUTPUT = '--output'
+COMMAND_LEARN = '--learn'
+COMMAND_RUN = '--run'
+
+# Validate that user's input params have the required ones
+def _validateMandatoryParameters(mandatoryParams, inputParams, message):
+  for param in mandatoryParams:
+    assert param in inputParams, message
+
+# Print all commands in output
+def _showHelp(commands):
+  print
+  for command in commands:
+    print '--' + command
+    print '\t' + commands[command]['definition']
+    print '\t' + 'Params: ' + ', '.join(commands[command]['params'])
+    print '\t' + 'Optional params: ' + \
+      ', '.join(commands[command]['optionalParams'])
+  print
+
+# Copy the template file to the path given
+def _generateTemplateFile(template, path):
+  copyfile('.templates/' + template, path)
+
+# Generate an input file from the template
+def _generateInputFile(path):
+  _generateTemplateFile('input_file_template.txt', path)
+
+# Generate an output file from the template
+def _generateOutputFile(path):
+  _generateTemplateFile('output_file_template.txt', path)
+
+# Transform the input file table format into matrixes 'x' and 'd'
+def _extractXD(tableLines):
+  x = list()
+  d = list()
+  rowCount = 0
+  for row in [l.strip().split(' ') for l in tableLines]:
+    xTemp = list()
+    dTemp = list()
+    inputs = True
+    for value in row:
+      if value == '|':
+        inputs = False
+      elif inputs:
+        xTemp.append(bool(int(value)))
+      else:
+        dTemp.append(float(value))
+    x.append(tuple(xTemp))
+    d.append(tuple(dTemp))
+
+    rowCount += 1
+
+  return (x, d)
+
+# Transform the output file table format into matrix 'x2'
+def _extractX2(tableLines):
+  x2 = list()
+  for row in [l.strip().split(' ') for l in tableLines]:
+    x2Temp = list()
+    for value in row:
+      x2Temp.append(bool(int(value)))
+    x2.append(x2Temp)
+  return x2
+
+# Read lines of a plain text file with # as comment lines, ignoring empty lines
+def _readTableFile(tableFilePath):
+  tableFile = open(tableFilePath, 'r')
+  table = tableFile.read().splitlines()
+  tableFile.close()
+
+  return [l for l in table if len(l.strip()) > 0 and l[0] != '#']
+
+# Read an input file and extract the table into matrixes 'x', 'd' and 'x2'
+def _readInputFile(inputFilePath):
+  return _extractXD(_readTableFile(inputFilePath))
+
+# Read an output file and extract the table into matrix 'x2'
+def _readOutputFile(outputFilePath):
+  return _extractX2(_readTableFile(outputFilePath))
+
+# Run command help
+def _runCommandHelp(parameters):
+  commandsFile = open('.json/commands.json', 'r')
+  _showHelp(loads(commandsFile.read()))
+  commandsFile.close()
+
+# Run command input
+def _runCommandInput(parameters):
+  _validateMandatoryParameters(['f'], parameters,
+    '--input needs parameter -f for the generated input file\'s name')
+  _generateInputFile(parameters['f'])
+
+# Run command output
+def _runCommandOutput(parameters):
+  _validateMandatoryParameters(['f'], parameters,
+    '--output needs parameter -f for the generated output file\'s name')
+  _generateOutputFile(parameters['f'])
+
+# Run command learn
+def _runCommandLearn(parameters):
+  _validateMandatoryParameters(['i', 'w'], parameters,
+    '--learn needs parameters -i and -w to get inputs and save learning')
+  constants_file = open('.json/constants.json', 'r')
+  constants = loads(constants_file.read())
+  constants_file.close()
+
+  (x, d) = _readInputFile(parameters['i'])
+  alpha = constants['alpha']
+  maxError = constants['maxError']
+  if 'a' in parameters:
+    alpha = float(parameters['a'])
+  if 'm' in parameters:
+    maxError = float(parameters['m'])
+
+  learningFile = open(parameters['w'], 'w')
+  learningFile.write(dumps(learningProcess(x, d, alpha, maxError)))
+  learningFile.close()
+
+# Run command run
+def _runCommandRun(parameters):
+  _validateMandatoryParameters(['w'], parameters,
+    '--run needs parameter -o to get learned weights')
+  learningFile = open(parameters['w'], 'r')
+  (n, m, l, wh, wo) = loads(learningFile.read())
+  learningFile.close()
+
+  x2 = []
+  if 'o' in parameters:
+    x2 = _readOutputFile(parameters['o'])
+  else:
+    x2 = list(product([False, True], repeat=n))
+
+  print '\n'.join([r for r in getY2(x2, n, m, l, wh, wo, True)])
+
+# Map received command with it's function
+def runCommand(command, parameters):
+  {
+    COMMAND_HELP: _runCommandHelp,
+    COMMAND_INPUT: _runCommandInput,
+    COMMAND_OUTPUT: _runCommandOutput,
+    COMMAND_LEARN: _runCommandLearn,
+    COMMAND_RUN: _runCommandRun
+  }[command](parameters)
--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import sys
 sys.path.append('libraries')
 
-from itertools import product
-from json import loads, dumps
-from shutil import copyfile
-from multilayer_perceptron import learningProcess, getY2
-
-# Constants
-COMMAND_HELP = '--help'
-COMMAND_INPUT = '--input'
-COMMAND_OUTPUT = '--output'
-COMMAND_LEARN = '--learn'
-COMMAND_RUN = '--run'
+from command_handler import runCommand
 
 # Capture dynamic parameters
 def getDynamicParameters(parameters):
@@ -20,137 +10,6 @@ def getDynamicParameters(parameters):
     assert k[0] == '-', 'ERROR: Wrong param ' + k
     parametersDict[k[1:]] = v
   return parametersDict
-
-# Validate that user's input params have the required ones
-def validateMandatoryParameters(mandatoryParams, inputParams, message):
-  for param in mandatoryParams:
-    assert param in inputParams, message
-
-# Print all commands in output
-def showHelp(commands):
-  print
-  for command in commands:
-    print '--' + command
-    print '\t' + commands[command]['definition']
-    print '\t' + 'Params: ' + ', '.join(commands[command]['params'])
-    print '\t' + 'Optional params: ' + \
-      ', '.join(commands[command]['optionalParams'])
-  print
-
-# Copy the template file to the path given
-def generateTemplateFile(template, path):
-  copyfile('.templates/' + template, path)
-
-# Generate an input file from the template
-def generateInputFile(path):
-  generateTemplateFile('input_file_template.txt', path)
-
-# Generate an output file from the template
-def generateOutputFile(path):
-  generateTemplateFile('output_file_template.txt', path)
-
-# Transform the input file table format into matrixes 'x' and 'd'
-def extractXD(tableLines):
-  x = list()
-  d = list()
-  rowCount = 0
-  for row in [l.strip().split(' ') for l in tableLines]:
-    xTemp = list()
-    dTemp = list()
-    inputs = True
-    for value in row:
-      if value == '|':
-        inputs = False
-      elif inputs:
-        xTemp.append(bool(int(value)))
-      else:
-        dTemp.append(float(value))
-    x.append(tuple(xTemp))
-    d.append(tuple(dTemp))
-
-    rowCount += 1
-
-  return (x, d)
-
-# Transform the output file table format into matrix 'x2'
-def extractX2(tableLines):
-  x2 = list()
-  for row in [l.strip().split(' ') for l in tableLines]:
-    x2Temp = list()
-    for value in row:
-      x2Temp.append(bool(int(value)))
-    x2.append(x2Temp)
-  return x2
-
-# Read lines of a plain text file with # as comment lines, ignoring empty lines
-def readTableFile(tableFilePath):
-  tableFile = open(tableFilePath, 'r')
-  table = tableFile.read().splitlines()
-  tableFile.close()
-
-  return [l for l in table if len(l.strip()) > 0 and l[0] != '#']
-
-# Read an input file and extract the table into matrixes 'x', 'd' and 'x2'
-def readInputFile(inputFilePath):
-  return extractXD(readTableFile(inputFilePath))
-
-# Read an output file and extract the table into matrix 'x2'
-def readOutputFile(outputFilePath):
-  return extractX2(readTableFile(outputFilePath))
-
-# Run command help
-def runCommandHelp(parameters):
-  commandsFile = open('.json/commands.json', 'r')
-  showHelp(loads(commandsFile.read()))
-  commandsFile.close()
-
-# Run command input
-def runCommandInput(parameters):
-  validateMandatoryParameters(['f'], parameters,
-    '--input needs parameter -f for the generated input file\'s name')
-  generateInputFile(parameters['f'])
-
-# Run command output
-def runCommandOutput(parameters):
-  validateMandatoryParameters(['f'], parameters,
-    '--output needs parameter -f for the generated output file\'s name')
-  generateOutputFile(parameters['f'])
-
-# Run command learn
-def runCommandLearn(parameters):
-  validateMandatoryParameters(['i', 'w'], parameters,
-    '--learn needs parameters -i and -w to get inputs and save learning')
-  constants_file = open('.json/constants.json', 'r')
-  constants = loads(constants_file.read())
-  constants_file.close()
-
-  (x, d) = readInputFile(parameters['i'])
-  alpha = constants['alpha']
-  maxError = constants['maxError']
-  if 'a' in parameters:
-    alpha = float(parameters['a'])
-  if 'm' in parameters:
-    maxError = float(parameters['m'])
-
-  learningFile = open(parameters['w'], 'w')
-  learningFile.write(dumps(learningProcess(x, d, alpha, maxError)))
-  learningFile.close()
-
-# Run command run
-def runCommandRun(parameters):
-  validateMandatoryParameters(['w'], parameters,
-    '--run needs parameter -o to get learned weights')
-  learningFile = open(parameters['w'], 'r')
-  (n, m, l, wh, wo) = loads(learningFile.read())
-  learningFile.close()
-
-  x2 = []
-  if 'o' in parameters:
-    x2 = readOutputFile(parameters['o'])
-  else:
-    x2 = list(product([False, True], repeat=n))
-
-  print '\n'.join([r for r in getY2(x2, n, m, l, wh, wo, True)])
 
 # Main function, receives the input arguments and determines the code to run
 def main(argv):
@@ -162,14 +21,8 @@ def main(argv):
   if len(argv) > 1:
     parameters = getDynamicParameters(argv[1:])
 
-  {
-    COMMAND_HELP: runCommandHelp,
-    COMMAND_INPUT: runCommandInput,
-    COMMAND_OUTPUT: runCommandOutput,
-    COMMAND_LEARN: runCommandLearn,
-    COMMAND_RUN: runCommandRun
-  }[command](parameters)
+  runCommand(command, parameters)
 
-
+# Map main function
 if __name__ == "__main__":
    main(sys.argv[1:])
