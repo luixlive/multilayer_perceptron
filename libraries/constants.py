592b14bb06f4b452b9523ae8bcdc247f554490e8
Luis Alfonso <luixlive@gmail.com>
2017-09-13 22:00:27 -0500

    Deep refactor main + command_handler

---
+++ libraries/constants.py
@@ -0,0 +1,17 @@
+CMD_HELP = '--help'
+CMD_INPUT = '--input'
+CMD_LEARN = '--learn'
+CMD_OUTPUT = '--output'
+CMD_RUN = '--run'
+
+CONST_ALPHA = 'alpha'
+CONST_MAX_ERR = 'maxError'
+
+PARAM_ALPHA = 'a'
+PARAM_FILE = 'f'
+PARAM_INPUT = 'i'
+PARAM_MAX_ERR = 'm'
+PARAM_OUTPUT = 'o'
+PARAM_ROUND = 'r'
+PARAM_SAVE = 's'
+PARAM_WEIGHTS = 'w'
--- main.py
+++ main.py
@@ -3,25 +3,9 @@ sys.path.append('libraries')
 
 from command_handler import runCommand
 
-# Capture dynamic parameters
-def getDynamicParameters(parameters):
-  parametersDict = dict()
-  for k, v in [p.split('=') if '=' in p else [p, True] for p in parameters]:
-    assert k[0] == '-', 'ERROR: Wrong param ' + k
-    parametersDict[k[1:]] = v
-  return parametersDict
-
-# Main function, receives the input arguments and determines the code to run
+# Main function, pass the argv to the command handle
 def main(argv):
-  if len(argv) == 0:
-    argv.append('--help')
-
-  command = argv[0]
-  parameters = dict()
-  if len(argv) > 1:
-    parameters = getDynamicParameters(argv[1:])
-
-  runCommand(command, parameters)
+  runCommand(argv) if len(argv) > 0 else runCommand(['--help'])
 
 # Map main function
 if __name__ == "__main__":
